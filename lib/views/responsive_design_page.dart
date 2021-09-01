3a8d535efd3d239017f7d821d3bc6c0125688d92
Murashu7 <murashu7@gmail.com>
2021-09-01 22:23:49 +0900

    ポートレートとランドスケープ対応

--- lib/views/responsive_design_page.dart
+++ lib/views/responsive_design_page.dart
@@ -20,83 +20,88 @@ class _Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    final isPortrait = height > width;
+
     return Center(
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 60,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 50.0,
-            ),
-            child: Text(
-              "Flutter is Google's UI toolkit for building beautiful, natively compiled",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                height: 1.85,
-                fontWeight: FontWeight.bold,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          maxWidth: 650,
+        ),
+        child: Column(
+          children: [
+            Spacer(),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 50.0,
               ),
-            ),
-          ),
-          const SizedBox(
-            height: 60.0,
-          ),
-          FlutterLogo(
-            size: 240,
-          ),
-          const SizedBox(
-            height: 60,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 50,
-            ),
-            child: Text(
-              "Fast Development Paint your app to life in milliseconds with Stateful Hot reload. Use a rich set of",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 15,
-                height: 1.85,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 60,
-          ),
-          SizedBox(
-            height: 42,
-            width: 300,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                textStyle: TextStyle(
+              child: Text(
+                "Flutter is Google's UI toolkit for building beautiful, natively compiled",
+                textAlign: TextAlign.center,
+                style: TextStyle(
                   color: Colors.white,
-                ),
-                primary: Theme.of(context).accentColor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(4),
+                  fontSize: 15 * (height + width) / (926 + 438),
+                  height: 1.85,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              onPressed: () {},
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 15,
+            ),
+            Spacer(),
+            if (isPortrait)
+              FlutterLogo(
+                size: height * 0.3,
+              ),
+            if (isPortrait) Spacer(),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 50,
+              ),
+              child: Text(
+                "Fast Development Paint your app to life in milliseconds with Stateful Hot reload. Use a rich set of",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15 * (height + width) / (926 + 438),
+                  height: 1.85,
                 ),
-                child: Text(
-                  "get started",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
+              ),
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    textStyle: TextStyle(
+                      color: Colors.white,
+                    ),
+                    primary: Theme.of(context).accentColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 15,
+                    ),
+                    child: Text(
+                      "get started",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 15 * (height + width) / (926 + 438),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+            Spacer(),
+          ],
+        ),
       ),
     );
   }
